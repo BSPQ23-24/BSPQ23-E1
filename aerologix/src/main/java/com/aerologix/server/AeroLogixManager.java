382afce198a5948ce732dc7611469e84d037bebf
Unai Igartua <unaiigartua@icloud.com>
2024-04-09 19:43:42 +0200

    Update AeroLogixManager.java

--- aerologix/src/main/java/com/aerologix/server/AeroLogixManager.java
+++ aerologix/src/main/java/com/aerologix/server/AeroLogixManager.java
@@ -2,12 +2,13 @@ package com.aerologix.server;
 
 import java.util.HashMap;
 import com.aerologix.serialization.*;
+import java.time.LocalDate;
 
 public class AeroLogixManager {
     
     protected HashMap<String, Booking> bookings;
 
-    protected HashMap<String, Flight> Passenger;
+    protected HashMap<String, Passenger> passenger;
 
     // Retrieve booking
     public Booking getBooking(String id) {
@@ -32,5 +33,34 @@ public class AeroLogixManager {
     public void deleteBooking(String id) {
         bookings.remove(id);
     }
+
+    // Retrieve Passenger
+    public Passenger getPassenger(String dni) {
+        return passenger.get(dni);
+    }
+
+    // Create Passenger
+    public void addPassenger(Passenger passenger) {
+        this.passenger.put(passenger.getDNI(), passenger);
+    }
+
+    // Modify Passenger
+    public void modifyPassenger(String dni, int phone, String name, String email, String nationality, LocalDate birthdate) {
+        Passenger passenger = getPassenger(dni);
+        passenger.setPhone(phone);
+        passenger.setName(name);
+        passenger.setEmail(email);
+        passenger.setNationality(nationality);
+        passenger.setBirthdate(birthdate);
+        this.passenger.replace(dni, passenger);
+    }
+
+    // Delete Passenger
+    public void deletePassenger(String dni) {
+        passenger.remove(dni);
+    }
+
+
+
 
 }
