b7ca7945fbcd828055d7c01086bbc3ee500041e3
Juan Orts <91051695+juanorts@users.noreply.github.com>
2024-04-28 15:04:56 +0200

    Bookings CRUD methods implementation

--- aerologix/src/main/java/com/aerologix/app/client/AeroLogixClient.java
+++ aerologix/src/main/java/com/aerologix/app/client/AeroLogixClient.java
@@ -130,6 +130,96 @@ public class AeroLogixClient {
             return null;
         }
     }
+
+    /*
+     * CRUD: booking
+     */
+
+    public int createBooking(String passengerDNI, int flightId, String userEmail, int airlineId) {
+        WebTarget registerUserWebTarget = webTarget.path("/booking/create");
+        Invocation.Builder invocationBuilder = registerUserWebTarget.request(MediaType.APPLICATION_JSON);
+
+        BookingData bookingData = new BookingData();
+        bookingData.setPassengerDNI(passengerDNI);
+        bookingData.setFlightId(flightId);
+        bookingData.setUserEmail(userEmail);
+        bookingData.setAirlineId(airlineId);
+
+        Response response = invocationBuilder.post(Entity.entity(bookingData, MediaType.APPLICATION_JSON));
+        if (response.getStatus() != Status.OK.getStatusCode()) {
+            logger.error("Cannot create a booking with data that does not exist in the database. Error code: {}", response.getStatus());
+            return -1;
+        } else {
+            logger.info("Booking correctly registered");
+            return 0;
+        }
+    }
+
+    public int deleteBooking(String bookingId) {
+        WebTarget deleteUserWebTarget = webTarget.path("/booking/delete");
+        Invocation.Builder invocationBuilder = deleteUserWebTarget.request(MediaType.APPLICATION_JSON);
+
+        Response response = invocationBuilder.post(Entity.entity(bookingId, MediaType.APPLICATION_JSON));
+        if(response.getStatus() != Status.OK.getStatusCode()) {
+            logger.error("There is no booking with id {}. Code: {}", bookingId, response.getStatus());
+            return -1;
+        } else {
+            logger.info("Booking '{}' deleted succesfully", bookingId);
+            return 0;
+        }
+    }
+
+    public int modifyBooking(int id, String passengerDNI, int flightId, String userEmail, int airlineId) {
+        WebTarget modifyUserWebTarget = webTarget.path("/booking/modify");
+        Invocation.Builder invocationBuilder = modifyUserWebTarget.request(MediaType.APPLICATION_JSON);
+
+        BookingData bookingData = new BookingData();
+        bookingData.setId(id);
+        bookingData.setPassengerDNI(passengerDNI);
+        bookingData.setFlightId(flightId);
+        bookingData.setUserEmail(userEmail);
+        bookingData.setAirlineId(airlineId);
+
+        Response response = invocationBuilder.post(Entity.entity(bookingData, MediaType.APPLICATION_JSON));
+        if(response.getStatus() != Status.OK.getStatusCode()) {
+            logger.error("There is no booking with that id. Code: {}", response.getStatus());
+            return -1;
+        } else {
+            logger.info("Booking '{}' modified succesfully", bookingData.getId());
+            return 0;
+        }
+    }
+
+    public BookingData getBooking(int id) {
+        WebTarget getUserWebTarget = webTarget.path("/booking/get").queryParam("id", id);
+        Invocation.Builder invocationBuilder = getUserWebTarget.request(MediaType.APPLICATION_JSON);
+        Response response = invocationBuilder.get();
+
+        if (response.getStatus() == Status.OK.getStatusCode()) {
+            logger.info("Booking retrieved succesfully");
+            return response.readEntity(BookingData.class);
+        } else {
+            logger.error("Failed to get booking '{}'. Status code: {}", id, response.getStatus());
+            return null;
+        }
+    }
+
+    public ArrayList<BookingData> getAllBookings() {
+        WebTarget getAllUsersWebTarget = webTarget.path("/booking/getAll");
+        Invocation.Builder invocationBuilder = getAllUsersWebTarget.request(MediaType.APPLICATION_JSON);
+        Response response = invocationBuilder.get();
+    
+        if (response.getStatus() == Status.OK.getStatusCode()) {
+            logger.info("All bookings retrieved successfully");
+            return response.readEntity(new GenericType<ArrayList<BookingData>>() {});
+        } else if (response.getStatus() == Status.NO_CONTENT.getStatusCode()) {
+            logger.error("No bookings registered");
+            return new ArrayList<BookingData>(); // Return an empty list if no bookings are registered
+        } else {
+            logger.error("Failed to get all bookings. Status code: {}", response.getStatus());
+            return null;
+        }
+    }
     
     // Main
     public static void main(String[] args) {
@@ -157,10 +247,28 @@ public class AeroLogixClient {
         //aerologixClient.deleteUser("admin");
 
         // Get all users
-        ArrayList<UserData> userList = aerologixClient.getAllUsers();
+        //ArrayList<UserData> userList = aerologixClient.getAllUsers();
         
-        for(UserData user : userList) {
-            System.out.println(user.getEmail());
-        }
+        //for(UserData user : userList) {
+        //    System.out.println(user.getEmail());
+        //}
+
+        // Create booking
+        //aerologixClient.createBooking("12345678A", 0, "prueba@mail.com", 0);
+        //aerologixClient.createBooking("00000000C", 0, "prueba@mail.com", 0);
+        //aerologixClient.deleteBooking("51");
+
+        // Modify booking
+        //aerologixClient.modifyBooking(71, "00000000B", 0, "juan@mail.es", 0);
+
+        // Get booking
+        //System.out.println(aerologixClient.getBooking(71));
+        
+        // Get all bookings
+        //ArrayList<BookingData> bookingList = aerologixClient.getAllBookings();
+        
+        //for(BookingData booking : bookingList) {
+        //    System.out.println(booking.getId());
+        //}
     }
 }
--- aerologix/src/main/java/com/aerologix/app/server/Server.java
+++ aerologix/src/main/java/com/aerologix/app/server/Server.java
@@ -129,8 +129,6 @@ public class Server {
                 user.setName(userData.getName());
                 user.setPassword(userData.getPassword());
                 user.setUserType(UserType.valueOf(userData.getUserType()));
-
-                pm.makePersistent(user);
                 logger.info("User modified: {}", user);
                 tx.commit();
                 return Response.ok().build();
@@ -272,34 +270,230 @@ public class Server {
      * CRUD: Booking
      */
 
-    @POST
+    @GET
     @Path("/booking/get")
-    public Response getBooking(int id) {
-        return null;
+    public Response getBooking(@QueryParam("id") String id) {
+        try{
+            tx.begin();
+            logger.info("Checking if the booking '{}' already exists or not...", id);
+            Booking booking = null;
+            
+            try { 
+                booking = pm.getObjectById(Booking.class, id);
+            } catch(JDOObjectNotFoundException e) {
+                logger.info("Booking with id '{}' does not exist in the database.", id);
+            }
+
+            tx.commit();
+
+            // If booking exists
+            if (booking != null) {
+                BookingData bookingData = new BookingData();
+                bookingData.setId(booking.getId());
+                bookingData.setPassengerDNI(booking.getPassenger().getDNI());
+                bookingData.setFlightId(booking.getFlight().getIdFlight());
+                bookingData.setUserEmail(booking.getUser().getEmail());
+                bookingData.setAirlineId(booking.getAirline().getId());
+                
+                logger.info("Sending bookingData to client...");
+                return Response.ok().entity(bookingData).build();
+            } else {
+                logger.info("Booking not found");
+                return Response.status(Response.Status.NOT_FOUND).entity("Booking not found").build();
+            }
+        }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+        }
     }
 
-    @POST
+    @GET
     @Path("/booking/getAll")
-    public Response getAllBookings() {
-        return null;
+    public Response getAllBooking() {
+        try{
+            tx.begin();
+
+            ArrayList<BookingData> bookingList = new ArrayList<BookingData>();
+
+            logger.info("Retrieving all bookings from database...");
+
+            Extent<Booking> e = pm.getExtent(Booking.class, true);
+            Iterator<Booking> iter=e.iterator();
+            while (iter.hasNext())
+            {
+                Booking booking = (Booking)iter.next();
+                BookingData bookingData = new BookingData();
+                bookingData.setId(booking.getId());
+                bookingData.setPassengerDNI(booking.getPassenger().getDNI());
+                bookingData.setFlightId(booking.getFlight().getIdFlight());
+                bookingData.setUserEmail(booking.getUser().getEmail());
+                bookingData.setAirlineId(booking.getAirline().getId());
+
+                bookingList.add(bookingData);
+            }
+            tx.commit();
+
+            if(bookingList.size() > 0){
+                logger.info("Sending all bookings to client...");
+                return Response.ok().entity(bookingList).build();
+            } else {
+                logger.error("No bookings registered");
+                return Response.status(Response.Status.NO_CONTENT).entity("No bookings registered").build();
+            }
+        }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+        }
     }
 
     @POST
     @Path("/booking/create")
     public Response createBooking(BookingData bookingData) {
-        return null;
+        try{
+            tx.begin();
+
+            boolean notFound = false;
+            
+            // Retrieve all linked classes
+            Passenger passenger = null;
+            Flight flight = null;
+            User user = null;
+            Airline airline = null;
+            
+            try { 
+                logger.info("Retrieving all data required for the booking");
+                passenger  = pm.getObjectById(Passenger.class, bookingData.getPassengerDNI());
+                flight = pm.getObjectById(Flight.class, bookingData.getFlightId());
+                user = pm.getObjectById(User.class, bookingData.getUserEmail());
+                airline = pm.getObjectById(Airline.class, bookingData.getAirlineId());
+            } catch(JDOObjectNotFoundException e) {
+                logger.info("At least one object of the Booking '{}' not found: {}", bookingData.getId(), e);
+                notFound = true;
+            }
+
+            // Create Booking instance and make it persistent
+            Booking booking = new Booking();
+            booking.setPassenger(passenger);
+            booking.setFlight(flight);
+            booking.setUser(user);
+            booking.setAirline(airline);
+
+            if(notFound) {
+                tx.commit();
+                logger.error("Error when creating a new booking. At least one of the objects related to the booking does not exist.");
+                return Response.status(Response.Status.UNAUTHORIZED).entity("Cannot create a new booking with data that does not exist in the database").build();
+            } else {
+                pm.makePersistent(booking);
+                tx.commit();
+                logger.info("Booking created succesfully");
+                return Response.ok().build();
+            }    
+        }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+        }
     }
 
     @POST
     @Path("/booking/modify")
     public Response modifyBooking(BookingData bookingData) {
-        return null;
+        try{
+            tx.begin();
+            // Get current booking data
+            logger.info("Get the current booking data for: {}", bookingData.getId());
+            Booking booking = null;
+            try { 
+                booking = pm.getObjectById(Booking.class, bookingData.getId());
+            } catch(JDOObjectNotFoundException e) {
+                logger.info("Booking with id '{}' does not exist in the database.", bookingData.getId());
+            }
+
+            if(booking != null) {
+                // Retrieve all linked classes
+                Passenger passenger = null;
+                Flight flight = null;
+                User user = null;
+                Airline airline = null;
+                try { 
+                    logger.info("Retrieving all data required for the modified booking");
+                    passenger  = pm.getObjectById(Passenger.class, bookingData.getPassengerDNI());
+                    flight = pm.getObjectById(Flight.class, bookingData.getFlightId());
+                    user = pm.getObjectById(User.class, bookingData.getUserEmail());
+                    airline = pm.getObjectById(Airline.class, bookingData.getAirlineId());
+                } catch(JDOObjectNotFoundException e) {
+                    logger.info("Could not find all the data for the new values: ", e);
+                }
+
+                // Modify all data except primary key (id)
+                booking.setId(bookingData.getId());
+                booking.setPassenger(passenger);
+                booking.setFlight(flight);
+                booking.setUser(user);
+                booking.setAirline(airline);
+                tx.commit();
+                logger.info("User modified: {}", user);
+                return Response.ok().build();
+            } else {
+                logger.error("There is no booking with id: {}", bookingData.getId());
+                tx.commit();
+                return Response.status(Response.Status.UNAUTHORIZED).entity("No booking found").build();
+            }
+            
+        }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+        }  
     }
 
     @POST
     @Path("/booking/delete")
     public Response deleteBooking(int id) {
-        return null;
+        try{
+            tx.begin();
+            logger.info("Checking if booking {} exists in the database...", id);
+            Booking booking = null;
+            try { 
+                booking = pm.getObjectById(Booking.class, id);
+            } catch(JDOObjectNotFoundException e) {
+                logger.info("Booking with id '{}' does not exist in the database.", id);
+            }
+
+            if(booking != null) {
+                // Delete user
+                pm.deletePersistent(booking);
+                logger.info("Booking deleted {}");
+                tx.commit();
+                return Response.ok().build();
+            } else {
+                logger.error("There is no booking with id: {}", id);
+                tx.commit();
+                return Response.status(Response.Status.UNAUTHORIZED).entity("No booking found").build();
+            }
+            
+        }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+        }
     }
 
     /*
