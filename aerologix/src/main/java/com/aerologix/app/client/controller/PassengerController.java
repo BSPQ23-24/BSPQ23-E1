568d0c7efcda1283a9279ca3e80e0eb115dc0c09
Unai Igartua <unaiigartua@icloud.com>
2024-05-01 11:49:50 +0200

    Passenger CRUD methods

---
+++ aerologix/src/main/java/com/aerologix/app/client/controller/PassengerController.java
@@ -0,0 +1,111 @@
+package com.aerologix.app.client.controller;
+
+import java.util.ArrayList;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import jakarta.ws.rs.client.*;
+import jakarta.ws.rs.core.*;
+import jakarta.ws.rs.core.Response.Status;
+
+import com.aerologix.app.client.AeroLogixClient;
+import com.aerologix.app.server.pojo.PassengerData;
+
+public class PassengerController {
+    
+    protected static final Logger logger = LogManager.getLogger();
+
+    private static PassengerController instance;
+
+    private PassengerController() {
+    }
+
+    public static synchronized PassengerController getInstance() {
+		if (instance == null) {
+			instance = new PassengerController();
+		}
+		return instance;
+	}
+
+
+    public int createPassenger(PassengerData passengerData) {
+        WebTarget registerPassengerWebTarget = AeroLogixClient.getInstance().getWebTarget().path("/passenger/register");
+        Invocation.Builder invocationBuilder = registerPassengerWebTarget.request(MediaType.APPLICATION_JSON);
+
+        logger.info("Sending POST request to server to register a new passenger...");
+        Response response = invocationBuilder.post(Entity.entity(passengerData, MediaType.APPLICATION_JSON));
+        if (response.getStatus() != Status.OK.getStatusCode()) {
+            logger.error("Passenger registration failed. Code: {}", response.getStatus());
+            return -1;
+        } else {
+            logger.info("Passenger correctly registered: {}", passengerData);
+            return 0;
+        }
+    }
+
+    public int modifyPassenger(PassengerData passengerData) {
+        WebTarget modifyPassengerWebTarget = AeroLogixClient.getInstance().getWebTarget().path("/passenger/modify");
+        Invocation.Builder invocationBuilder = modifyPassengerWebTarget.request(MediaType.APPLICATION_JSON);
+
+        logger.info("Sending POST request to server to modify passenger with DNI '{}'...", passengerData.getDNI());
+        Response response = invocationBuilder.post(Entity.entity(passengerData, MediaType.APPLICATION_JSON));
+        if(response.getStatus() != Status.OK.getStatusCode()) {
+            logger.error("Passenger modification failed. Code: {}", response.getStatus());
+            return -1;
+        } else {
+            logger.info("Passenger '{}' modified successfully", passengerData.getDNI());
+            return 0;
+        }
+    }
+
+    public int deletePassenger(String dni) {
+        WebTarget deletePassengerWebTarget = AeroLogixClient.getInstance().getWebTarget().path("/passenger/delete");
+        Invocation.Builder invocationBuilder = deletePassengerWebTarget.request(MediaType.APPLICATION_JSON);
+
+        logger.info("Sending POST request to server to delete passenger with DNI '{}'...", dni);
+        Response response = invocationBuilder.post(Entity.entity(dni, MediaType.APPLICATION_JSON));
+        if(response.getStatus() != Status.OK.getStatusCode()) {
+            logger.error("Passenger deletion failed. Code: {}", response.getStatus());
+            return -1;
+        } else {
+            logger.info("Passenger '{}' deleted successfully", dni);
+            return 0;
+        }
+    }
+
+    public PassengerData getPassenger(String dni) {
+        WebTarget getPassengerWebTarget = AeroLogixClient.getInstance().getWebTarget().path("/passenger/get").queryParam("dni", dni);
+        Invocation.Builder invocationBuilder = getPassengerWebTarget.request(MediaType.APPLICATION_JSON);
+        
+        logger.info("Sending GET request to server to retrieve passenger with DNI '{}'...", dni);
+        Response response = invocationBuilder.get();
+
+        if (response.getStatus() == Status.OK.getStatusCode()) {
+            logger.info("Passenger retrieved successfully");
+            return response.readEntity(PassengerData.class);
+        } else {
+            logger.error("Failed to get passenger '{}'. Status code: {}", dni, response.getStatus());
+            return null;
+        }
+    }
+
+    public ArrayList<PassengerData> getAllPassengers() {
+        WebTarget getAllPassengersWebTarget = AeroLogixClient.getInstance().getWebTarget().path("/passenger/getAll");
+        Invocation.Builder invocationBuilder = getAllPassengersWebTarget.request(MediaType.APPLICATION_JSON);
+        
+        logger.info("Sending GET request to server to retrieve all passengers...");
+        Response response = invocationBuilder.get();
+    
+        if (response.getStatus() == Status.OK.getStatusCode()) {
+            logger.info("All passengers retrieved successfully");
+            return response.readEntity(new GenericType<ArrayList<PassengerData>>() {});
+        } else if (response.getStatus() == Status.NO_CONTENT.getStatusCode()) {
+            logger.error("No passengers registered");
+            return new ArrayList<PassengerData>(); // Return an empty list if no passengers are registered
+        } else {
+            logger.error("Failed to get all passengers. Status code: {}", response.getStatus());
+            return null;
+        }
+    }
+}
---
+++ aerologix/src/main/java/com/aerologix/app/server/service/PassengerService.java
@@ -0,0 +1,213 @@
+package com.aerologix.app.server.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.jdo.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
+
+import com.aerologix.app.server.pojo.PassengerData;
+import com.aerologix.app.server.AeroLogixServer;
+import com.aerologix.app.server.jdo.Passenger;
+
+@Path("/aerologix")
+@Produces(MediaType.APPLICATION_JSON)
+public class PassengerService {
+    
+    protected static final Logger logger = LogManager.getLogger();
+
+	private PersistenceManagerFactory pmf;
+	private PersistenceManager pm;
+	private Transaction tx;
+
+    public PassengerService() {
+		this.pmf = AeroLogixServer.getInstance().getPersistenceManagerFactory();
+		this.pm = pmf.getPersistenceManager();
+		this.tx = pm.currentTransaction();
+    }
+
+    // CRUD methods
+
+	@POST
+	@Path("/passenger/register")
+	public Response registerPassenger(PassengerData passengerData) {
+		try {
+			tx.begin();
+			logger.info("Checking if passenger '{}' already exists or not", passengerData.getDNI());
+			Passenger passenger = null;
+
+			try {
+				passenger = pm.getObjectById(Passenger.class, passengerData.getDNI());
+			} catch (JDOObjectNotFoundException e) {
+				logger.info("Passenger with DNI '{}' does not exist in the database.", passengerData.getDNI());
+			}
+
+			if (passenger == null) {
+				logger.info("Creating passenger...");
+				passenger = new Passenger(passengerData.getDNI(), passengerData.getPhone(), passengerData.getName(),
+                        passengerData.getEmail(), passengerData.getNationality(), passengerData.getBirthdate());
+				pm.makePersistent(passenger);
+				logger.info("Passenger created: {}", passenger);
+				tx.commit();
+				return Response.ok().build();
+			} else {
+				logger.error("DNI '{}' is already in use", passengerData.getDNI());
+				tx.commit();
+				return Response.status(Response.Status.UNAUTHORIZED).entity("DNI is already in use").build();
+			}
+
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+		}
+	}
+
+	@POST
+	@Path("/passenger/modify")
+	public Response modifyPassenger(PassengerData passengerData) {
+		try {
+			tx.begin();
+			logger.info("Get the current passenger data for: {}", passengerData.getDNI());
+			Passenger passenger = null;
+			try {
+				passenger = pm.getObjectById(Passenger.class, passengerData.getDNI());
+			} catch (JDOObjectNotFoundException e) {
+				logger.info("Passenger with DNI '{}' does not exist in the database.", passengerData.getDNI());
+			}
+
+			if (passenger != null) {
+				passenger.setPhone(passengerData.getPhone());
+				passenger.setName(passengerData.getName());
+				passenger.setEmail(passengerData.getEmail());
+				passenger.setNationality(passengerData.getNationality());
+				passenger.setBirthdate(passengerData.getBirthdate());
+				logger.info("Passenger modified: {}", passenger);
+				tx.commit();
+				return Response.ok().build();
+			} else {
+				logger.error("There is no passenger registered with DNI: {}", passengerData.getDNI());
+				tx.commit();
+				return Response.status(Response.Status.UNAUTHORIZED).entity("No passenger found").build();
+			}
+
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+		}
+	}
+
+	@POST
+	@Path("/passenger/delete")
+	public Response deletePassenger(String dni) {
+		try {
+			tx.begin();
+			logger.info("Checking if passenger {} exists in the database...", dni);
+			Passenger passenger = null;
+			try {
+				passenger = pm.getObjectById(Passenger.class, dni);
+			} catch (JDOObjectNotFoundException e) {
+				logger.info("Passenger with DNI '{}' does not exist in the database.", dni);
+			}
+
+			if (passenger != null) {
+				pm.deletePersistent(passenger);
+				logger.info("Passenger deleted: {}", dni);
+				tx.commit();
+				return Response.ok().build();
+			} else {
+				logger.error("There is no passenger registered with DNI: {}", dni);
+				tx.commit();
+				return Response.status(Response.Status.UNAUTHORIZED).entity("No passenger found").build();
+			}
+
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+		}
+	}
+
+	@GET
+	@Path("/passenger/get")
+	public Response getPassenger(@QueryParam("dni") String dni) {
+		try {
+			tx.begin();
+			logger.info("Checking if the passenger '{}' already exists or not...", dni);
+			Passenger passenger = null;
+
+			try {
+				passenger = pm.getObjectById(Passenger.class, dni);
+			} catch (JDOObjectNotFoundException e) {
+				logger.info("Passenger with DNI '{}' does not exist in the database.", dni);
+			}
+
+			tx.commit();
+
+			if (passenger != null) {
+				PassengerData passengerData = new PassengerData();
+				passengerData.setDNI(passenger.getDNI());
+				passengerData.setPhone(passenger.getPhone());
+				passengerData.setName(passenger.getName());
+				passengerData.setEmail(passenger.getEmail());
+				passengerData.setNationality(passenger.getNationality());
+				passengerData.setBirthdate(passenger.getBirthdate());
+
+				logger.info("Sending passengerData to client...");
+				return Response.ok().entity(passengerData).build();
+			} else {
+				logger.info("Passenger not found");
+				return Response.status(Response.Status.NOT_FOUND).entity("Passenger not found").build();
+			}
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+		}
+	}
+
+	@GET
+	@Path("/passenger/getAll")
+	public Response getAllPassengers() {
+		try {
+			tx.begin();
+
+			ArrayList<PassengerData> passengerList = new ArrayList<>();
+
+			logger.info("Retrieving all passengers from database...");
+
+			Extent<Passenger> e = pm.getExtent(Passenger.class, true);
+			Iterator<Passenger> iter = e.iterator();
+			while (iter.hasNext()) {
+				Passenger passenger = iter.next();
+				PassengerData passengerData = new PassengerData();
+				passengerData.setDNI(passenger.getDNI());
+				passengerData.setPhone(passenger.getPhone());
+				passengerData.setName(passenger.getName());
+				passengerData.setEmail(passenger.getEmail());
+				passengerData.setNationality(passenger.getNationality());
+				passengerData.setBirthdate(passenger.getBirthdate());
+				passengerList.add(passengerData);
+			}
+			tx.commit();
+
+			if (passengerList.size() > 0) {
+				logger.info("Sending all passengers to client...");
+				return Response.ok().entity(passengerList).build();
+			} else {
+				logger.error("No passengers registered");
+				return Response.status(Response.Status.NO_CONTENT).entity("No passengers registered").build();
+			}
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+		}
+	}
+}
