9ac6857ba4a356dd1253100eca72601af5d5bff4
Ander Hernández <ander.hernandez@opendeusto.es>
2024-05-18 11:57:51 +0200

    Fixing Booking service's performance test

--- aerologix/src/test/java/com/aerologix/app/server/service/BookingServicePerfTest.java
+++ aerologix/src/test/java/com/aerologix/app/server/service/BookingServicePerfTest.java
@@ -8,8 +8,10 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
+import javax.jdo.Extent;
 import javax.jdo.JDOObjectNotFoundException;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
@@ -67,9 +69,8 @@ public class BookingServicePerfTest {
     @Test
     @JUnitPerfTest(threads = 10, durationMs = 2000)
     public void testCreateBooking() {
-    	Flight flightMock = mock(Flight.class);
-    	Set<Booking> bookingsMock = new HashSet<>();
         BookingData bookingData = new BookingData();
+        Flight flightMock = mock(Flight.class);
         bookingData.setId(2);
         bookingData.setPassengerDNI("79050089D");
         bookingData.setFlightId(1);
@@ -78,7 +79,6 @@ public class BookingServicePerfTest {
 
         when(pmMock.getObjectById(Booking.class, 2)).thenThrow(new JDOObjectNotFoundException());
         when(pmMock.getObjectById(Flight.class, 1)).thenReturn(flightMock);
-        when(flightMock.getBookings()).thenReturn(bookingsMock);
         Response response = bookingService.createBooking(bookingData);
 
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
@@ -86,36 +86,62 @@ public class BookingServicePerfTest {
         verify(txMock, times(1)).commit();
     }
     
-    /* @Test
-     @JUnitPerfTest(threads = 10, durationMs = 2000)
+   
+    @Test
+    @JUnitPerfTest(threads = 10, durationMs = 2000)
     public void testGetBooking() {
-    	Booking bookingMock = mock(Booking.class);
+        Booking bookingMock = mock(Booking.class);
+        Flight flightMock = mock(Flight.class);
+        User userMock = mock(User.class);
+        Airline airlineMock = mock(Airline.class);
+        Passenger passengerMock = mock(Passenger.class);
+        Set<Booking> bookingsMock = new HashSet<>();
+        bookingsMock.add(mock(Booking.class));
+        bookingsMock.add(mock(Booking.class));
+
         when(bookingMock.getId()).thenReturn(2);
-        when(bookingMock.getPassenger().getDNI()).thenReturn("79050089D");
+        when(bookingMock.getFlight()).thenReturn(flightMock);
+        when(flightMock.getBookings()).thenReturn(bookingsMock);
+        when(bookingMock.getPassenger()).thenReturn(passengerMock);
+        when(bookingMock.getUser()).thenReturn(userMock);
+        when(bookingMock.getAirline()).thenReturn(airlineMock);
+        when(passengerMock.getDNI()).thenReturn("79050089D");
         when(pmMock.getObjectById(Booking.class, 2)).thenReturn(bookingMock);
 
-        Response response = bookingService.getBooking("2");
+        Response response = bookingService.getBooking(2);
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
         BookingData bookingData = (BookingData) response.getEntity();
-        assertEquals(2, BookingData.getId());
+        assertEquals(2, bookingData.getId());
         assertEquals("79050089D", bookingData.getPassengerDNI());
-    } */
-    
-    /* @Test
+    }
+     @Test
      @JUnitPerfTest(threads = 10, durationMs = 2000)
     public void testGetAllBookings() {
     	Extent<Booking> extentMock = mock(Extent.class);
         Iterator<Booking> iteratorMock = mock(Iterator.class);
         Booking bookingMock = mock(Booking.class);
+        Flight flightMock = mock(Flight.class);
+        User userMock = mock(User.class);
+        Airline airlineMock = mock(Airline.class);
+        Passenger passengerMock = mock(Passenger.class);
+        Set<Booking> bookingsMock = new HashSet<>();
+        bookingsMock.add(mock(Booking.class));
+        bookingsMock.add(mock(Booking.class));
 
         when(pmMock.getExtent(Booking.class, true)).thenReturn(extentMock);
+        when(bookingMock.getFlight()).thenReturn(flightMock);
+        when(flightMock.getBookings()).thenReturn(bookingsMock);
+        when(bookingMock.getPassenger()).thenReturn(passengerMock);
+        when(bookingMock.getUser()).thenReturn(userMock);
+        when(bookingMock.getAirline()).thenReturn(airlineMock);
         when(extentMock.iterator()).thenReturn(iteratorMock);
         when(iteratorMock.hasNext()).thenReturn(true, false);
         when(iteratorMock.next()).thenReturn(bookingMock);
+        
 
         Response response = bookingService.getAllBookings();
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-    } */
+    } 
     
     @Test
     @JUnitPerfTest(threads = 10, durationMs = 2000)
